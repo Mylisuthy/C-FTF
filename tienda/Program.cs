a06c05d234a3155c025378f3f7364df97d120d39
Mylisuthy <juanch052048@gmail.com>
2025-09-08 13:52:57 -0500

    Add files via upload

---
+++ tienda/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace tienda;
+
+public class pedidos;
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("bienvenido a la tienda.");
+
+        Console.WriteLine("como te llamas?");
+        string nombre = Console.ReadLine();
+
+        Console.WriteLine("cuantas manzanas quiere?");
+        int manzanas = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("cuanto cuesta cada manzana?");
+        double precioManzana = double.Parse(Console.ReadLine());
+
+        double totalManzana = manzanas * precioManzana;
+
+        Console.WriteLine("quieres bolsa plastica true/false");
+        bool quiereBolsa = bool.Parse(Console.ReadLine());
+
+        double precioBolsa = 100;
+        double total = totalManzana;
+
+        if (quiereBolsa)
+        {
+            total += precioBolsa;
+        }
+        
+    }
+}
