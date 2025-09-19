b10ae2394cc2c3ba5d5989a0f26f67652c6e5c13
Mylisuthy <juanch052048@gmail.com>
2025-09-19 13:52:13 -0500

    Delete RiwiMusic directory

--- RiwiMusic/Models/Tiket.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RiwiMusic.Models;
-
-public class Tiket
-{
-    public int IdTiket { get; set; }
-    public int IdCustomer { get; set; }
-    public string IdConcert { get; set; }
-    public string InfTiket { get; set; }
-}
